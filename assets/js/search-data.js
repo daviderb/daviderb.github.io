1cd0d4c12c778b193665cabc9b1fda6664dd6eaf
daviderb <88505876+daviderb@users.noreply.github.com>
2026-02-23 10:32:02 +0000

    Deploying to gh-pages from @ daviderb/daviderb.github.io@40a8fcd995ba6c2c2e680217231c2a6d52451cac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-check-out-our-new-blog-post-on-auditing-differential-privacy-software-sparkles",
           title: 'Check out our new blog post on auditing differential privacy software!  :sparkles:',
           description: "",
